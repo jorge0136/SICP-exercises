cc0eaf8d17acf1798f76eec782c199a374e08eb6
Thomas George <jorge0136@gmail.com>
2020-05-12 17:28:31 -0700

    Adds unit testing for books implementation

---
+++ week 2/SICP-exercise1.7-test.rkt
@@ -0,0 +1,37 @@
+#lang racket/base
+
+(require rackunit
+          "SICP-exercise1.7.rkt")
+
+;; Books implementation of good-enough?
+    (define seven-decimal-delta .0000001)
+    (define six-decimal-delta .000001)
+    (define five-decimal-delta .00001)
+    (define three-decimal-delta .001)
+    (define two-decimal-delta .01)
+    (define one-decimal-delta .1)
+
+;; Very Small Numbers:
+    (check-= (books-sqrt 5.0) 2.2360679775 six-decimal-delta "S1: books-sqrt of `5`.")
+    (check-= (books-sqrt 0.5) 0.70710678118 five-decimal-delta "S2: books-sqrt of `0.5`.")
+    (check-= (books-sqrt 0.05) 0.22360679775 three-decimal-delta "S3: books-sqrt of `0.05`.")
+    (check-= (books-sqrt 0.005) 0.07071067811 two-decimal-delta "S4: books-sqrt of `0.005`.")
+    (check-= (books-sqrt 0.0005) 0.02236067977 one-decimal-delta "S5: books-sqrt of `0.0005`.")
+
+  ;; ***********************************************************************************************
+
+  ;; Very Large Numbers:
+    (check-= (books-sqrt 500.0) 22.360679775 seven-decimal-delta "L1:books-sqrt of `500`.")
+    (check-= (books-sqrt 5000.0) 70.7106781187 seven-decimal-delta "L2: books-sqrt of `5000`.")
+    (check-= (books-sqrt 50000.0) 223.60679775 seven-decimal-delta "L3: books-sqrt of `50000`.")
+    (check-= (books-sqrt 500000.0) 707.106781187 six-decimal-delta "L4: books-sqrt of `500000`.")
+    (check-= (books-sqrt 5000000.0) 2236.0679775 six-decimal-delta "L5: books-sqrt of `5000000`.")
+    (check-= (books-sqrt 50000000.0) 7071.06781187 six-decimal-delta "L6: books-sqrt of `50000000`.")
+    (check-= (books-sqrt 500000000000000000.0) 707106781.187 three-decimal-delta "L6: books-sqrt of `500000000000000000`.")
+
+    ;; These square roots do finish calculating but they are wildly inaccurate.
+
+    ;; (check-= (books-sqrt 500000000000000000000000000000000000000000000000000000000.0)
+    ;;           2.236068e+28 100.0 "L7: books-sqrt of `500000000000000000000000000000000000000000000000000000000`.")
+    ;; (check-= (books-sqrt 50000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000.0)
+    ;;           2.236068e+56 100.0 "L8: books-sqrt of `50000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000`.")
