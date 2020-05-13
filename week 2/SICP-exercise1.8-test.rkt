4a5e67530a0c291a469266e0a376ce1533eb49df
Thomas George <jorge0136@gmail.com>
2020-05-13 11:57:25 -0700

    Adds cube-root exercise

--- week 2/SICP-exercise1.8-test.rkt
+++ week 2/SICP-exercise1.8-test.rkt
@@ -1,5 +1,11 @@
-#lang sicp
-;; Exercise 1.8: Newton's method for cube roots is based on the fact that if y is an approximation
-;; to the cube root of x, then a better approximation is given by the value
-;; ;; x / y square + 2y all over 3.
-;; Use this formula to implement a cube-root procedure analogous to the square-root procedure.
+#lang racket/base
+
+(require rackunit
+          "SICP-exercise1.8.rkt")
+
+  (define five-decimal-delta .00001)
+  (define three-decimal-delta .001)
+
+  (check-= (cube-root 0.8) 0.92831776672 three-decimal-delta "1: cube-root of `0.8`.")
+  (check-= (cube-root 8.0) 2.0 five-decimal-delta "2: cube-root of `8.0`.")
+  (check-= (cube-root 80.0) 4.30886938006 three-decimal-delta "3: cube-root of `80.0`.")
--- week 2/SICP-exercise1.8.rkt
+++ week 2/SICP-exercise1.8.rkt
@@ -1,8 +1,9 @@
-#lang sicp
-;; Exercise 1.8: Newton's method for cube roots is based on the fact that if y is an approximation 
-;; to the cube root of x, then a better approximation is given by the value 
-;; ;; x / y square + 2y all over 3. 
-;; Use this formula to implement a cube-root procedure analogous to the square-root procedure. 
+#lang racket
+(require racket/trace)
+;; Exercise 1.8: Newton's method for cube roots is based on the fact that if y is an approximation
+;; to the cube root of x, then a better approximation is given by the value
+;; ;; x / guess squared + 2guess all over 3.
+;; Use this formula to implement a cube-root procedure analogous to the square-root procedure.
 
 
   (define ( square x ) (* x x))
@@ -11,16 +12,18 @@
                         x))
 
   (define (improve guess x)
-    (/ 3.0 
-       (/ x (+ (square guess) 
-            (* guess 2.0)))))
+    (/ (+ (* guess
+             2.0)
+          (/ x
+             (square guess)))
+       3.0))
 
   (define (improved-guess-delta old_guess x)
     (- (improve old_guess x)
         old_guess))
-        
+
   (define (good-enough? guess x)
-    (< 
+    (<
        (abs (improved-guess-delta guess x))
        (* guess 0.001)))
 
@@ -32,5 +35,9 @@
   (define (cube-root x)
     (cube-root-iter 1.0 x))
 
+  (trace improve)
+  (trace good-enough?)
 
-  (cube-root 8.0)
+;; Provide the cube root function for testing.
+(provide cube-root)
+
